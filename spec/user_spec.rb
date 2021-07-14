5d3e975c99a92205e38ff5c2f951af9178f1d8c6
Michael Mattson <michael.p.mattson@gmail.com>
2021-07-14 14:33:18 -0600

    Add learn test

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -21,4 +21,19 @@ RSpec.describe User do
       expect(user_1.jokes).to eq([])
     end
   end
+
+  context '#learn' do
+    it 'adds a joke to user.jokes' do
+      user_1 = User.new("Sal")
+      joke_1 = Joke.new(22, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+      joke_2 = Joke.new(13, "How do you keep a lion from charging?", "Take away its credit cards.")
+
+      expect(user_1.jokes).to eq([])
+
+      user_1.learn(joke_1)
+      user_1.learn(joke_2)
+
+      expect(user_1.jokes).to eq([joke1, joke_2])
+    end
+  end
 end
