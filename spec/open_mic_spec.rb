c98056c77e3f1bad63fc8018947635791b183e19
Michael Mattson <michael.p.mattson@gmail.com>
2021-07-14 15:20:27 -0600

    Add repeated jokes test

--- spec/open_mic_spec.rb
+++ spec/open_mic_spec.rb
@@ -44,4 +44,28 @@ RSpec.describe OpenMic do
       expect(open_mic.performers).to eq([user_1, user_2])
     end
   end
+
+  context '#repeated_jokes?' do
+    it 'tells if performers have any of the same jokes' do
+      open_mic = OpenMic.new({location: "Comedy Works", date: "11-20-18"})
+
+      user_1 = User.new("Sal")
+      user_2 = User.new("Ali")
+
+      open_mic.welcome(user_1)
+      open_mic.welcome(user_2)
+
+      joke_1 = Joke.new(22, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+      joke_2 = Joke.new(13, "How do you keep a lion from charging?", "Take away its credit cards.")
+
+      user_2.learn(joke_1)
+      user_2.learn(joke_2)
+
+      expect(open_mic.repeated_jokes?).to be(false)
+
+      user_1.learn(joke_1)
+
+      expect(open_mic.repeated_jokes?).to be(true)
+    end
+  end
 end
