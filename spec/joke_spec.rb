240d06d5343f3d5444b8f07c81175fc0af47c119
Michael Mattson <michael.p.mattson@gmail.com>
2021-07-14 14:20:20 -0600

    Fix tests

--- spec/joke_spec.rb
+++ spec/joke_spec.rb
@@ -11,19 +11,19 @@ RSpec.describe Joke do
     it 'has an id' do
       joke = Joke.new(22, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
 
-      expect(joke.id).to be(22)
+      expect(joke.id).to eq(22)
     end
 
-    xit 'has a setup' do
+    it 'has a setup' do
       joke = Joke.new(22, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
 
-      expect(joke.setup).to be("Why did the strawberry cross the road?")
+      expect(joke.setup).to eq("Why did the strawberry cross the road?")
     end
 
     xit 'has a punchline' do
       joke = Joke.new(22, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
 
-      expect(joke.punchline).to be("Because his mother was in a jam.")
+      expect(joke.punchline).to eq("Because his mother was in a jam.")
     end
   end
 end
